7b6291fc18c1c1848a2b1c1ddc54d75b24c1b0a9
“Kha <hoangkhahoa@gmail.com>
2023-04-13 22:52:08 +0700

    remove unused data

--- lib/res/constants.dart
+++ lib/res/constants.dart
@@ -5,43 +5,7 @@ class MyConstants {
 
   static double heightAppBar = 60;
 
-  static String myName = "Hhoa.";
-
-  static String linkCV = "https://drive.google.com/file/d/1aW4LMf1fT0udpMw5wNyAYQmQm3-youCY/view?usp=drivesdk";
-
-  static String linkFb = "https://www.facebook.com/hoangkhahoa";
-
-  static String linkSkype = "https://join.skype.com/invite/n8I2LGeAlRnC";
-
-  static String linkLinkedin = "https://www.linkedin.com/in/hoa-hoang-35944012b/";
-
-  static String linkGithub = "https://github.com/hhoa";
-
   static double paddingHorizontal = 50;
 
   static double paddingVertical = 60;
-
-  static String descDailyDeal = "Landing page of Daily Deal\nOne of the most important screen at Sendo";
-
-  static String descFlashSale = "Landing page of Flash Sale\nOne of the most important screen at Sendo";
-
-  static String descGos = "Game hub based on Songs with various artists and songs";
-
-  static String descLmx = "Game 'Lac Mua Xu' running at Sendo";
-
-  static String descCake = "Cake - Digital Banking built with Flutter";
-
-  static String linkCake = "https://cake.vn/";
-
-  static String descLuckySale = "Game 'Vong Quay Bay Gia' recently launch at Sendo";
-
-  static String linkDailyDeal = "https://www.sendo.vn/su-kien/deal-soc-moi-ngay";
-
-  static String linkFlashSale = "https://www.sendo.vn/flash-sale/";
-
-  static String linkGos = "https://apps.apple.com/vn/app/game-of-songs-music-games/id1492267122?l=vi";
-
-  static String linkLmx = "http://sendo.onelink.me/dihK/c1fa8733";
-
-  static String linkLuckySale = "https://sendo.onelink.me/Udvq/2f39c673";
 }
--- lib/res/images.dart
+++ lib/res/images.dart
@@ -2,32 +2,4 @@ class MyAssetImages {
   MyAssetImages._();
 
   static String imageProfile = "assets/images/profile_undraw.png";
-
-  static String imageBe = "assets/images/be_logo.png";
-
-  static String imageSendo = "assets/images/sendo_logo.png";
-
-  static String imageAioz = "assets/images/aioz_logo.png";
-
-  static String imageBosch = "assets/images/bosch_logo.jpeg";
-
-  static String imageCakeHomescreen = "assets/images/cake_home_screen.PNG";
-
-  static String imageDailyDeal = "assets/images/dailydeal_screen.jpeg";
-
-  static String imageFlashSale = "assets/images/flashsale_screen.jpeg";
-
-  static String imageGos = "assets/images/gos_screen.PNG";
-
-  static String imageLmx = "assets/images/lmx_screen.jpeg";
-
-  static String imageLuckySale = "assets/images/luckysale_screen.jpeg";
-
-  static String imageFacebook = "assets/images/ic_facebook.png";
-
-  static String imageGithub = "assets/images/ic_github.png";
-
-  static String imageLinkedin = "assets/images/ic_linkedin.png";
-
-  static String imageSkype = "assets/images/ic_skype.png";
 }
